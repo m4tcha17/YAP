98cdcbb727caee07b35c50bdb76701d13df670fb
MetsysLlifer <metsysllifer@gmail.com>
2025-12-15 01:35:19 +0800

    Merge remote-tracking branch 'origin/menu'

---
+++ Objects/portal.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var collision: CollisionShape2D = $collision
+
+signal scene_change
+@export var scene_name: String = "..."
+
+func _ready() -> void:
+	connect("body_entered", teleport)
+
+
+func teleport(node) -> void:
+	if node.is_in_group("player"):
+		#get_tree().change_scene_to_file("res://Scenes/EmptySpace.tscn")
+		emit_signal("scene_change")
